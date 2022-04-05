64f7e374de5a6f02da81a5530d7d3261a9a79bc9
niiife1 <Ivandemirov28@gmail.com>
2022-04-05 11:40:07 +0300

    initial commit

--- task1.py
+++ task1.py
@@ -1,3 +1,4 @@
+from collections import  Counter
 message = input()
 
 while True:
@@ -6,22 +7,29 @@ while True:
     if name == "Reveal" :
         break
     func = command[1]
-    #  last_func = command[2]
 
     if name == "InsertSpace":
         message = list(message)
         message.insert(int(func), " ")
-        message = "".join(new)
+        print("".join(message))
+        continue
+
+    elif name == "Reverse":
+        if func in message:
+            cutted_word = list((Counter(message)-Counter(func)).elements())
+            func = list(func)
+            func.reverse()
+            message = cutted_word + func
+            print("".join(message))
+        else:
+            print("error")
+
+    elif name == "ChangeAll":
+        last_func = command[2]
+        message = message.replace(func, last_func)
         print(message)
 
-    if name == "Reverse":
-        if func in message:
-            final_string = list(message) - list(func)
-            func.reverse()
-            message.append(func)
-
-    # if name == "ChangeAll":
-
-print(message)
+x = "".join(message)
+print(f'You have a new text message: {x}')
 
 # Hello, I was here
